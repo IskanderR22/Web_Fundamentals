fbdbea1ba11fb317e46ddf8c8f06a29f661f353b
Iskander Rangel <icerangel@outlook.com>
2021-06-25 12:20:33 -0700

    W1D5 Finished

--- algo.js
+++ algo.js
@@ -16,3 +16,30 @@ for(var i = 0; i < 7; i++){
 }
 
 console.log("there are " + countPositives + " positive values");
+
+
+
+// REVERSE AN ARRAY
+// CREATE A FUNCTION WHERE GIVEN AN ARRAY YOU REVERSE THE ARRAY AND RETURN THE ARRAY
+// ["a", "b", "c", "d", "e"] => ["e", "d", "c", "b", "a"]
+
+// CREATE A FUNCTION WRAPPER (1 param / array type)
+// ITERATE THROUGHT ARRAY (FOR LOOP)
+// NEED A TEMP VARIABLE TO STORE A VALUE
+// SWAP THE VALUES WITHIN THE ARRAY
+// RETURN THE ARRAY
+
+var array = ["a", "b", "c", "d", "e"];
+
+
+function ReversePro(array){
+        var temp;
+    var last = array.length - 1;
+    for(var i = 0; i < array.length/2; i++){
+        temp = array[i];
+        array[i] = array[last];
+        array[last] = temp;
+        --last;
+    }
+    return array;
+}
