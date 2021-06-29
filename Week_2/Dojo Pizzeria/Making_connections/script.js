00be9338e1a5b23d7975032c638a74fa6c683ef9
Iskander Rangel <icerangel@outlook.com>
2021-06-29 13:49:19 -0700

    W2 D2

--- Week_2/Dojo Pizzeria/Making_connections/script.js
+++ Week_2/Dojo Pizzeria/Making_connections/script.js
@@ -1,6 +1,39 @@
 
-var profileName = document.querySelector("profileName");
+
+var btn = 2;
+var connections = 500;
 
 function changeName(){
-    profileName.innerHTML = "Ice"
+    var profileName = document.querySelector("#profileName");
+    profileName.innerText = "Ice";
 }
+
+function removeTop(imgTag){
+    var removeTopRequest = document.querySelector("#topConnection");
+    var buttonOne = document.querySelector(".buttonOne");
+    removeTopRequest.remove();
+    btn--;
+    buttonOne.innerText = btn;
+    if(imgTag.id == "firstAccept" || imgTag.id == "secondAccept"){
+        connections++;
+        var buttonTwo = document.querySelector(".buttonTwo");
+        buttonTwo.innerText = connections;
+    }
+
+}
+
+
+function removeBottom(imgTag){
+    var removeBottomRequest = document.querySelector("#bottomRequest");
+    var buttonOne = document.querySelector(".buttonOne");
+    removeBottomRequest.remove();
+    btn--;
+    buttonOne.innerText = btn;
+    if(imgTag.id == "firstAccept" || imgTag.id == "secondAccept"){
+        connections++;
+        var buttonTwo = document.querySelector(".buttonTwo");
+        buttonTwo.innerText = connections;
+    }
+}
+
+
